f579032ecf43d659e49317f23bbb373d3a9d8f34
小C (clawd-xsl) <clawd.xsl@gmail.com>
2026-06-20 04:01:39 -0700

    fix: use description instead of value for ISO to avoid object serialization

--- web/src/core/exif-metadata/exif-metadata.ts
+++ web/src/core/exif-metadata/exif-metadata.ts
@@ -27,14 +27,10 @@ class ExifMetadata {
       : undefined;
     this.fNumber = metadata?.FNumber?.description?.substring(0, 5)?.replace('f/', 'F');
 
-    // Fix: ISOSpeedRatings.value can be an array or object in some formats (PNG, etc.)
-    if (metadata?.ISOSpeedRatings?.value) {
-      const isoVal = metadata.ISOSpeedRatings.value;
-      const isoNum = Array.isArray(isoVal) ? isoVal[0] : (typeof isoVal === 'object' ? metadata.ISOSpeedRatings.description : isoVal);
-      this.iso = isoNum !== undefined ? 'ISO' + String(isoNum) : undefined;
-    } else if (metadata?.ISOSpeedRatings?.description) {
-      this.iso = 'ISO' + metadata.ISOSpeedRatings.description;
-    }
+    // Fix: always use description for ISO - it's a reliable string in ExifReader
+    this.iso = metadata?.ISOSpeedRatings?.description
+      ? 'ISO' + metadata.ISOSpeedRatings.description
+      : undefined;
 
     this.exposureTime = metadata?.ExposureTime?.description ? metadata?.ExposureTime?.description + 's' : undefined;
     this.thumbnail = metadata?.Thumbnail?.base64 ? 'data:image/jpg;base64,' + metadata?.Thumbnail?.base64 : undefined;
@@ -49,7 +45,6 @@ class ExifMetadata {
         this.takenAt = metadata.DateTimeOriginal.description;
       }
     } else if (metadata?.DateTimeOriginal?.value) {
-      // Some formats store value as array
       const val = Array.isArray(metadata.DateTimeOriginal.value) ? metadata.DateTimeOriginal.value[0] : metadata.DateTimeOriginal.value;
       if (typeof val === 'string') {
         const parts = val.split(' ');
