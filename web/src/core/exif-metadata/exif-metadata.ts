3117341f5f4b2ae69a2627f704e0714f5e43f2c8
小C (clawd-xsl) <clawd.xsl@gmail.com>
2026-06-20 03:57:39 -0700

    fix: handle ISO array/object and date parsing for PNG EXIF

--- web/src/core/exif-metadata/exif-metadata.ts
+++ web/src/core/exif-metadata/exif-metadata.ts
@@ -26,14 +26,46 @@ class ExifMetadata {
         : `${metadata.UprightFocalLength35mm.value}mm`
       : undefined;
     this.fNumber = metadata?.FNumber?.description?.substring(0, 5)?.replace('f/', 'F');
-    this.iso = metadata?.ISOSpeedRatings?.value ? 'ISO' + metadata?.ISOSpeedRatings?.value?.toString() : undefined;
+
+    // Fix: ISOSpeedRatings.value can be an array or object in some formats (PNG, etc.)
+    if (metadata?.ISOSpeedRatings?.value) {
+      const isoVal = metadata.ISOSpeedRatings.value;
+      const isoNum = Array.isArray(isoVal) ? isoVal[0] : (typeof isoVal === 'object' ? metadata.ISOSpeedRatings.description : isoVal);
+      this.iso = isoNum !== undefined ? 'ISO' + String(isoNum) : undefined;
+    } else if (metadata?.ISOSpeedRatings?.description) {
+      this.iso = 'ISO' + metadata.ISOSpeedRatings.description;
+    }
+
     this.exposureTime = metadata?.ExposureTime?.description ? metadata?.ExposureTime?.description + 's' : undefined;
     this.thumbnail = metadata?.Thumbnail?.base64 ? 'data:image/jpg;base64,' + metadata?.Thumbnail?.base64 : undefined;
 
     if (metadata?.DateTimeOriginal?.description) {
-      const yyyymmdd = metadata.DateTimeOriginal.description.split(' ')[0].split(':').join('-');
-      const hhmmss = metadata.DateTimeOriginal.description.split(' ')[1];
-      this.takenAt = `${yyyymmdd} ${hhmmss}`;
+      const parts = metadata.DateTimeOriginal.description.split(' ');
+      if (parts.length >= 2) {
+        const yyyymmdd = parts[0].split(':').join('-');
+        const hhmmss = parts[1];
+        this.takenAt = `${yyyymmdd} ${hhmmss}`;
+      } else {
+        this.takenAt = metadata.DateTimeOriginal.description;
+      }
+    } else if (metadata?.DateTimeOriginal?.value) {
+      // Some formats store value as array
+      const val = Array.isArray(metadata.DateTimeOriginal.value) ? metadata.DateTimeOriginal.value[0] : metadata.DateTimeOriginal.value;
+      if (typeof val === 'string') {
+        const parts = val.split(' ');
+        if (parts.length >= 2) {
+          this.takenAt = parts[0].split(':').join('-') + ' ' + parts[1];
+        } else {
+          this.takenAt = val;
+        }
+      }
+    } else if (metadata?.DateTime?.description) {
+      const parts = metadata.DateTime.description.split(' ');
+      if (parts.length >= 2) {
+        const yyyymmdd = parts[0].split(':').join('-');
+        const hhmmss = parts[1];
+        this.takenAt = `${yyyymmdd} ${hhmmss}`;
+      }
     }
   }
 }
